f67afd85287c09c52ffe36bd26a19ca80933b793
Lam Zi Hao <lamzi@amazon.com>
2023-07-05 21:10:47 +0800

    add website build

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ app.add_middleware(CORSMiddleware, allow_methods=["*"], allow_headers=["*"], all
 def inference_endpoint_handler(body: InferenceRequestSchema):
     return inference_handler(body.question)
 
+from fastapi.staticfiles import StaticFiles
+
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope):
+        response = await super().get_response(path, scope)
+        if response.status_code == 404:
+            response = await super().get_response('.', scope)
+        return response
+
+app.mount('/', SPAStaticFiles(directory='folder', html=True), name='Helpdesk Website')
 
 if __name__ == "__main__":
     uvicorn.run(app, port=80, host="0.0.0.0", reload=True)
